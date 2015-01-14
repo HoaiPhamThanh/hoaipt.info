a181b36222ea191a2c7187b0975f0a2a2f24777e
hoai_pt <hoai_pt@septeni-technology.jp>
2015-01-14 15:49:14 +0700

    layout 1

---
+++ app/Controller/BlogsController.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Blog Controller
+ *
+ * @property User $User
+ */
+class BlogsController extends AppController {
+
+/**
+ * index method
+ *
+ * @return void
+ */
+public $layout = 'blog';
+public function index() {
+
+	}
+}
